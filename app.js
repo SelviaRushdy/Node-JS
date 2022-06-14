3357e32fef9f82537d80b66196d51c178601b1c6
SelviaRushdy <46251313+SelviaRushdy@users.noreply.github.com>
2022-06-14 19:20:03 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const passport = require('passport');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 
-const port=process.env.port || 3000;
+const port=process.env.port;
 const app=express();
 
 app.use(morgan('combined'));
